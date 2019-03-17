ca01731d4dbd4767aa91a8b6da865b52b76020e6
raiou <c.s.t0474981366@gmail.com>
2019-03-17 10:06:08 +0000

    repair

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:show]
   
-  def index
-    @users = User.all.page(params[:page])
-  end
+  # def index
+  #   @users = User.all.page(params[:page])
+  # end
 
   def show
     @user = User.find(params[:id])
